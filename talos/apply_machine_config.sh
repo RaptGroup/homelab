901be23a803e15bc55d5ee76f07c84cf4e7fce0d
Jack Hall <jhall@scalecomputing.com>
2025-08-20 07:56:35 -0500

    fix: templated script for applying controlplane and worker network configurations

--- talos/apply_machine_config.sh
+++ talos/apply_machine_config.sh
@@ -1,5 +1,7 @@
-CONTROL_PLANE_IP="192.168.4.236"
-WORKER_IP=("192.168.4.238" "192.168.4.237" "192.168.4.239" "192.168.4.243" "192.168.4.242")
+#!/bin/bash
+CONTROL_PLANE_IP="192.168.4.244"
+CONTROL_PLANE_NODES=("192.168.4.244" "192.168.4.250" "192.168.4.251")
+WORKER_IP=("192.168.4.248" "192.168.4.245" "192.168.4.249" "192.168.4.246" "192.168.4.247")
 
 CLUSTER_NAME="rockingham-k8s"
 DISK_NAME="vda"
@@ -10,7 +12,28 @@ DISK_NAME="vda"
 # echo "Applying control plane configuration"
 # talosctl apply-config --insecure --nodes $CONTROL_PLANE_IP --file controlplane.yaml
 #
+for ip in "${CONTROL_PLANE_NODES[@]}"; do
+    echo "Applying config to worker node: $ip"
+    # talosctl apply-config --insecure --nodes "$ip" --file worker.yaml
+    MAC_ADDRESS=$(talosctl get links --nodes "$ip" --talosconfig talosconfig | grep 'ens3' | awk '{print $7}')
+    talosctl apply-config \
+        --nodes "$ip" \
+        --file controlplane.yaml \
+        --talosconfig talosconfig \
+        --config-patch "[{\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/addresses/0\", \"value\": \"$ip/24\"},
+                       {\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/deviceSelector/hardwareAddr\", \"value\": \"$MAC_ADDRESS\"}]" # --dry-run \
+
+done
+
 for ip in "${WORKER_IP[@]}"; do
     echo "Applying config to worker node: $ip"
-    talosctl apply-config --insecure --nodes "$ip" --file worker.yaml
+    # talosctl apply-config --insecure --nodes "$ip" --file worker.yaml
+    MAC_ADDRESS=$(talosctl get links --nodes "$ip" --talosconfig talosconfig | grep 'ens3' | awk '{print $7}')
+    talosctl apply-config \
+        --nodes "$ip" \
+        --file worker.yaml \
+        --talosconfig talosconfig \
+        --config-patch "[{\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/addresses/0\", \"value\": \"$ip/24\"},
+                       {\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/deviceSelector/hardwareAddr\", \"value\": \"$MAC_ADDRESS\"}]" # --dry-run \
+
 done
