9709ff16a7c0b47626b7a24ae3371ec9aaf4c343
Jack Hall <jhall@scalecomputing.com>
2025-08-24 07:49:28 -0500

    chore: rebuild talos cluster

--- talos/apply_machine_config.sh
+++ talos/apply_machine_config.sh
@@ -1,39 +1,76 @@
 #!/bin/bash
-CONTROL_PLANE_IP="192.168.4.244"
-CONTROL_PLANE_NODES=("192.168.4.244" "192.168.4.250" "192.168.4.251")
-WORKER_IP=("192.168.4.248" "192.168.4.245" "192.168.4.249" "192.168.4.246" "192.168.4.247")
+CONTROL_PLANE_IP="192.168.4.85"
+ADDITIONAL_CONTROL_PLANE_NODES=("192.168.4.86" "192.168.4.87")
+WORKER_IP=("192.168.4.164" "192.168.4.124" "192.168.4.254" "192.168.4.83" "192.168.4.84")
 
 CLUSTER_NAME="rockingham-k8s"
 DISK_NAME="vda"
 
 # echo "Generating Talos config for cluster: $CLUSTER_NAME"
 # talosctl gen config $CLUSTER_NAME https://$CONTROL_PLANE_IP:6443 --install-disk /dev/$DISK_NAME
+
+# echo "###########################################################"
+# echo "########## INITIAL CONTROL-PLANE CONFIGURATION ############"
+# echo "###########################################################"
+# echo "Applying initial control plane configuration..."
+# echo "Applying controlplane config to node: $CONTROL_PLANE_IP"
+# MAC_ADDRESS=$(talosctl get links --insecure --nodes "$CONTROL_PLANE_IP" --talosconfig talosconfig | grep 'ens3' | awk '{print $6}')
+# talosctl apply-config \
+#     --insecure \
+#     --nodes "$CONTROL_PLANE_IP" \
+#     --file controlplane.yaml \
+#     --talosconfig talosconfig \
+#     --config-patch "[{\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/addresses/0\", \"value\": \"$CONTROL_PLANE_IP/24\"},
+#                 {\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/deviceSelector/hardwareAddr\", \"value\": \"$MAC_ADDRESS\"}]"
+
+# echo "#############################################################"
+# echo "########### WORKER NODES CONFIGURATION ######################"
+# echo "#############################################################"
+# for ip in "${WORKER_IP[@]}"; do
+#     echo "Applying config to worker node: $ip"
+#     MAC_ADDRESS=$(talosctl get --insecure links --nodes "$ip" --talosconfig talosconfig | grep 'ens3' | awk '{print $6}')
+#     talosctl apply-config \
+#         --insecure \
+#         --nodes "$ip" \
+#         --file worker.yaml \
+#         --talosconfig talosconfig \
+#         --config-patch "[{\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/addresses/0\", \"value\": \"$ip/24\"},
+#                        {\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/deviceSelector/hardwareAddr\", \"value\": \"$MAC_ADDRESS\"}]" # --dry-run \
 #
-# echo "Applying control plane configuration"
-# talosctl apply-config --insecure --nodes $CONTROL_PLANE_IP --file controlplane.yaml
-#
-for ip in "${CONTROL_PLANE_NODES[@]}"; do
-    echo "Applying config to worker node: $ip"
-    # talosctl apply-config --insecure --nodes "$ip" --file worker.yaml
-    MAC_ADDRESS=$(talosctl get links --nodes "$ip" --talosconfig talosconfig | grep 'ens3' | awk '{print $7}')
+# done
+
+# echo "#############################################################"
+# echo "########### SET TALOSCONFIG CLUSTER ENDPOINT ################"
+# echo "#############################################################"
+# talosctl --talosconfig=./talosconfig config endpoints $CONTROL_PLANE_IP
+
+# echo "#############################################################"
+# echo "########### BOOTSTRAP CONTROLPLANE ETCD CLUSTER #############"
+# echo "#############################################################"
+# talosctl bootstrap --nodes $CONTROL_PLANE_IP --talosconfig=./talosconfig
+
+# echo "#############################################################"
+# echo "########### SET KUBECONFIG AND CONTEXT ######################"
+# echo "#############################################################"
+# talosctl kubeconfig --nodes $CONTROL_PLANE_IP --talosconfig=./talosconfig
+
+# echo "#############################################################"
+# echo "################## CHECK CLUSTER HEALTH #####################"
+# echo "#############################################################"
+# talosctl --nodes $CONTROL_PLANE_IP --talosconfig=./talosconfig health
+
+# echo "#############################################################"
+# echo "############ REGISTER ADDITIONAL CONTROLPLANE NODES #########"
+# echo "#############################################################"
+for ip in "${ADDITIONAL_CONTROL_PLANE_NODES[@]}"; do
+    echo "Applying config to additional control plane node: $ip"
+    MAC_ADDRESS=$(talosctl get links --insecure --nodes "$ip" --talosconfig talosconfig | grep 'ens3' | awk '{print $6}')
     talosctl apply-config \
         --nodes "$ip" \
+        --insecure \
         --file controlplane.yaml \
         --talosconfig talosconfig \
         --config-patch "[{\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/addresses/0\", \"value\": \"$ip/24\"},
-                       {\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/deviceSelector/hardwareAddr\", \"value\": \"$MAC_ADDRESS\"}]" # --dry-run \
-
-done
-
-for ip in "${WORKER_IP[@]}"; do
-    echo "Applying config to worker node: $ip"
-    # talosctl apply-config --insecure --nodes "$ip" --file worker.yaml
-    MAC_ADDRESS=$(talosctl get links --nodes "$ip" --talosconfig talosconfig | grep 'ens3' | awk '{print $7}')
-    talosctl apply-config \
-        --nodes "$ip" \
-        --file worker.yaml \
-        --talosconfig talosconfig \
-        --config-patch "[{\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/addresses/0\", \"value\": \"$ip/24\"},
-                       {\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/deviceSelector/hardwareAddr\", \"value\": \"$MAC_ADDRESS\"}]" # --dry-run \
+                       {\"op\": \"replace\", \"path\": \"/machine/network/interfaces/0/deviceSelector/hardwareAddr\", \"value\": \"$MAC_ADDRESS\"}]"
 
 done
